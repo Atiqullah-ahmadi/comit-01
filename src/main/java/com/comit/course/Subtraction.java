caf77a6b11876544f2fbd9f8100bdd67df714dd8
Atiqullah Ahmadi <at.infoid@gmail.com>
2023-02-18 15:50:24 -0500

    Subtraction class added

---
+++ src/main/java/com/comit/course/Subtraction.java
@@ -0,0 +1,11 @@
+package com.comit.course;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is something good");
+
+	}
+
+}
