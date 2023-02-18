d4fae5e6c81b77ede4960c55a1f128dc74b8e2f1
Atiqullah Ahmadi <at.infoid@gmail.com>
2023-02-17 19:11:09 -0500

    first version

---
+++ src/main/java/com/comit/course/App.java
@@ -0,0 +1,19 @@
+package com.comit.course;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a =20;
+    	
+    	if (a>10) {
+    		System.out.println("The number is greater than ten");
+    	}else {
+        System.out.println( "Hello World!" + a );
+    	}
+    }
+}
---
+++ src/main/java/com/comit/course/Variable.java
@@ -0,0 +1,8 @@
+package com.comit.course;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		System.out.println("can this massage run");
+	}
+}
